7903a7aa6aadd05a406ea43310e58abd68134665
Ben B <ben@kwartzlab.ca>
2020-01-31 16:32:51 +0000

    Add vendor assets

---
+++ resources/views/vendor/mail.old/html/message.blade.php
@@ -0,0 +1,27 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            Kwartzlab Makerspace
+        @endcomponent
+    @endslot
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            Sent {{ date('Y-m-d') }} by {{ config('app.name') }}
+        @endcomponent
+    @endslot
+@endcomponent
