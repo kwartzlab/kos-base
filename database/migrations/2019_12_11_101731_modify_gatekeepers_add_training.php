c486eb5503f1c9f846444f133ca9592ec3507a3b
Shift <shift@laravelshift.com>
2022-08-24 17:30:52 +0000

    Adopt anonymous migrations

--- database/migrations/2019_12_11_101731_modify_gatekeepers_add_training.php
+++ database/migrations/2019_12_11_101731_modify_gatekeepers_add_training.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ModifyGatekeepersAddTraining extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -37,4 +37,4 @@ class ModifyGatekeepersAddTraining extends Migration
             $table->dropColumn('training_prereq');
         });
     }
-}
+};
--- database/migrations/2020_01_22_144317_modify_gatekeeper_add_photo_sharedauth.php
+++ database/migrations/2020_01_22_144317_modify_gatekeeper_add_photo_sharedauth.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ModifyGatekeeperAddPhotoSharedauth extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class ModifyGatekeeperAddPhotoSharedauth extends Migration
             $table->dropColumn('shared_auth');
         });
     }
-}
+};
