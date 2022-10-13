0c583e851d54ae99a76f64d92cde5fc31a41ee3c
Ben Brown <ben@kwartzlab.ca>
2022-10-13 23:25:46 +0000

    Update report to use preferred names

--- app/Console/Commands/SendActiveMembersEmail.php
+++ app/Console/Commands/SendActiveMembersEmail.php
@@ -41,14 +41,14 @@ class SendActiveMembersEmail extends Command
         $this->info('Generating list of active members...');
 
         // get all active users
-        $users = \App\Models\User::where('status', 'active')->orderby('first_name')->orderby('last_name')->get();
+        $users = \App\Models\User::where('status', 'active')->orderby('first_preferred')->orderby('last_preferred')->get();
 
         // find authentications from the past month, if more than 3 add them to the list
         $email_data['member_list'] = [];
         foreach ($users as $user) {
             $recs = \App\Models\Authentication::where('user_id', $user['id'])->where('gatekeeper_id', '11')->where('created_at', '>=', Carbon::now()->subMonth()->toDateTimeString())->get();
             if (count($recs) >= 3) {
-                $email_data['member_list'][] = $user['first_name'].' '.$user['last_name'];
+                $email_data['member_list'][] = $user['first_preferred'].' '.$user['last_preferred'];
             }
         }
 
