c07ae7f6393e94f0792bd770391f27e961767114
christian <c.griffin@vehikl.com>
2023-11-09 21:03:50 -0500

    add user and role table seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,12 +7,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
+    private static int $memberIdIncrement = 1;
+
     public function definition(): array
     {
         static $password;
 
         $firstName = $this->faker->firstName();
         $lastName = $this->faker->lastName();
+
+        self::$memberIdIncrement++;
 
         return [
             'first_name' => $firstName,
@@ -22,7 +26,7 @@ class UserFactory extends Factory
             'email' => "$firstName.$lastName@example.com",
             'password' => $password ?: $password = bcrypt('secret'),
             'status' => 'active',
-            'member_id' => User::query()->max('member_id') + 1,
+            'member_id' => self::$memberIdIncrement,
             'acl' => '',
             'date_applied' => now()->subDays(14),
             'date_admitted' => now()->subDays(7),
@@ -39,6 +43,16 @@ class UserFactory extends Factory
             'notes' => $this->faker->paragraph(),
             'remember_token' => $this->faker->lexify('???????????????'),
         ];
+    }
+
+    public function status(string $status)
+    {
+        if(method_exists($this, $status)) {
+            $factory = $this->{$status}();
+        }
+
+        $factory = $factory ?? $this;
+        return $factory->state(['status' => $status]);
     }
 
     public function active()
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,30 +2,89 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\RolePermission;
+use App\Models\User;
+use App\Models\UserStatus;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        $this->call(RoleTableSeeder::class);
+        $this->seedRolesTable();
+        $this->seedUsersTable();
     }
-}
 
-class RoleTableSeeder extends Seeder
-{
-    public function run()
+    private function seedRolesTable(): void
     {
+        Role::query()->create(['name' => Role::ROLE_SUPERUSER_NAME, 'description' => '']);
+        $bodRole = Role::query()->create(['name' => Role::ROLE_BOD_NAME, 'description' => '']);
+        $keyFobAssignerRole = Role::query()->create(['name' => Role::ROLE_KEY_FOB_ASSIGNER_NAME, 'description' => '']);
+        $bookkeeperRole = Role::query()->create(['name' => Role::ROLE_BOOKKEEPER_NAME, 'description' => '']);
 
-        // Create superuser role and give it to first user account
-        DB::table('roles')->delete();
-        \App\Models\Role::create(['name' => 'Superusers', 'description' => 'Access to all KwartzlabOS functions']);
-        \App\Models\UserRole::create(['user_id' => '1', 'role_id' => '1']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'keys', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'users', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'gatekeepers', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'teams', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'reports', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'roles', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bodRole->id, 'object' => 'forms', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $keyFobAssignerRole->id, 'object' => 'keys', 'operation' => 'manage']);
+        RolePermission::query()->create(['role_id' => $bookkeeperRole->id, 'object' => 'users', 'operation' => 'manage']);
+    }
+
+    private function seedUsersTable(): void
+    {
+        $superuserUser = User::factory()->active()->create([
+            'first_name' => 'Superuser',
+            'last_name' => 'Superuser',
+            'email' => 'superuser-dev@kwartzlab.ca',
+        ]);
+
+        $superuserUser->roles(true)
+            ->attach(Role::query()->where('name', Role::ROLE_SUPERUSER_NAME)->firstOrFail());
+
+        $bodUser = User::factory()->active()->create([
+            'first_name' => 'BoD',
+            'last_name' => 'BoD',
+            'email' => 'bod-dev@kwartzlab.ca',
+        ]);
+
+        $bodUser->roles(true)
+            ->attach(Role::query()->where('name', Role::ROLE_BOD_NAME)->firstOrFail());
+
+        $keyFobAssignerUser = User::factory()->active()->create([
+            'first_name' => 'Key Fob Assigner',
+            'last_name' => 'Key Fob Assigner',
+            'email' => 'kfa-dev@kwartzlab.ca',
+        ]);
+
+        $keyFobAssignerUser->roles(true)
+            ->attach(Role::query()->where('name', Role::ROLE_KEY_FOB_ASSIGNER_NAME)->firstOrFail());
+
+        $bookkeeperUser = User::factory()->active()->create([
+            'first_name' => 'Bookkeeper',
+            'last_name' => 'Bookkeeper',
+            'email' => 'bookkeeper-dev@kwartzlab.ca',
+        ]);
+
+        $bookkeeperUser->roles(true)
+            ->attach(Role::query()->where('name', Role::ROLE_BOOKKEEPER_NAME)->firstOrFail());
+
+        $this->createUsers(UserStatus::STATUS_ACTIVE, 50);
+        $this->createUsers(UserStatus::STATUS_INACTIVE, 20);
+        $this->createUsers(UserStatus::STATUS_SUSPENDED, 10);
+        $this->createUsers(UserStatus::STATUS_TERMINATED, 10);
+        $this->createUsers(UserStatus::STATUS_INACTIVE_ABANDONED, 5);
+        $this->createUsers(UserStatus::STATUS_HIATUS, 5);
+        $this->createUsers(UserStatus::STATUS_APPLICANT_ABANDONED, 10);
+        $this->createUsers(UserStatus::STATUS_APPLICANT_DENIED, 5);
+        $this->createUsers(UserStatus::STATUS_APPLICANT, 5);
+    }
+
+    private function createUsers(string $status, int $count = 1): void
+    {
+        User::factory()->count($count)->status($status)->create();
     }
 }
