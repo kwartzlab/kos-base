bf4c087e10a584ddf09c0f71b4e5d37fdf291ffc
Ian Edington <ian.edington@gmail.com>
2023-12-31 23:50:22 -0500

    Serve the active user count publicly (#17)
    The request was made to publish the active user count for the smirrl
    flip counters and our https://spaceapi.io listing.
    
    This has not been approved by the board, so not sure if this needs
    approval
    
    fixes #14

---
+++ app/Http/Controllers/PublicInfoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class PublicInfoController extends Controller
+{
+    /**
+     * Share information publicly about Kwartzlab as json
+     */
+    public function index(): JsonResponse
+    {
+        $data = [
+            'number_of_active_users' => $this->getPublicUserCount(),
+        ];
+
+        return response()->json($data);
+    }
+
+    /**
+     * We want to share the number of active users publicly, but we don't want people to obsess over the ebbs and flows of our active member count.
+     * We'll remove 10 from the count and round to the nearest 10, this will give us a conservative but useful number.
+     *
+     * Justification for the obfuscation: https://kwartzlab.slack.com/archives/CBT57TWN4/p1702126237514579
+     */
+    public function getPublicUserCount(): int
+    {
+        $count = User::where('status', 'active')->count();
+        $count = max(0, $count - 10); // Ensure count is not negative
+        return round($count / 10) * 10;
+    }
+}
---
+++ tests/Feature/Http/Controllers/PublicInfoControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PublicInfoControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @dataProvider publicUserCountData
+     */
+    public function testPublicUserCount(int $activeUsers, int $expectedUserCount, int $hiatusUsers = 0): void
+    {
+        User::factory($activeUsers)->active()->create();
+        User::factory($hiatusUsers)->hiatus()->create();
+
+        $this->get('/api/public-info')
+            ->assertStatus(200)
+            ->assertJson([
+                'number_of_active_users' => $expectedUserCount,
+            ]);
+    }
+
+    /**
+     * @return int[][]
+     */
+    public function publicUserCountData(): array
+    {
+        return [
+            // [activeUsers, expectedUserCount, hiatusUsers = 0]
+
+            // does not fail when there are 0 users
+            [0, 0],
+
+            // rolls over to the next multiple of ten when the count is greater by 5
+            [14, 0],
+            [15, 10],
+
+            // hiatus members don't affect count
+            [15, 10, 10],
+        ];
+    }
+}
