a5c5c9ec62e60e067748c1458c3b9972128ec924
Ian Edington <ian.edington@gmail.com>
2024-05-21 18:57:53 -0400

    chore(deploy): Current deploy script
    This is a first stab at a deploy script. It follows the Laravel docs
    and attempts to be reasonably fault tolerant.
    This is not result in a no downtime deploy, which in kOS's current
    state is acceptable.
    
    refs:
    - https://laravel.com/docs/10.x/deployment
    - https://hakk.dev/blog/posts/www-data-git-deployment/

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# usage: sudo -u www-data deploy.sh
+
+# Bash Strict Mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "Deployment started ..."
+
+git fetch origin main
+
+php artisan down || true
+
+git merge --ff origin/main
+
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+php artisan cache:clear
+php artisan optimize:clear
+php artisan optimize
+
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
