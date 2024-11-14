fa1c9dd8c16925f92ae671c63c9bfb42219213d6
everett-ripley <everett.ripley@gmail.com>
2024-11-14 15:25:05 -0500

    Added doorway seeding

--- database/seeders/GatekeepersTableSeeder.php
+++ database/seeders/GatekeepersTableSeeder.php
@@ -9,16 +9,20 @@ use Illuminate\Database\Seeder;
 class GatekeepersTableSeeder extends Seeder
 {
 
-    private array $names = ["Band Saw", "Laser Cutter", "Table Saw", "3D Printer"];
+    private array $tool_names = ["Band Saw", "Laser Cutter", "Table Saw", "3D Printer"];
+    private array $door_names = ["Door 1", "Door 2", "Door 3", "Door 4"];
     public function run()
     {
-     foreach ($this->names as $name) {
-        $this->createGatekeeper(1, $name);
+     foreach ($this->tool_names as $name) {
+        $this->createGatekeeper(1, $name, "lockout");
      } 
+     foreach ($this->door_names as $name) {
+        $this->createGatekeeper(1, $name, "doorway");
+     }
     }
 
-    private function createGatekeeper(int $count = 1, string $name): void
+    private function createGatekeeper(int $count = 1, string $name, string $type): void
     {
-        GateKeeper::factory()->count($count)->create(["name" => $name]);
+        GateKeeper::factory()->count($count)->create(["name" => $name, "type" => $type]);
     }
 }
