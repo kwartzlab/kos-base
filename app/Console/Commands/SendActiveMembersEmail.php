2853e5748a7d671f133620295b8c0328e2d79803
Verdi R-D <vrodriguesdiamond@petabloc.com>
2024-11-21 12:51:55 -0500

    feat: include rear door authorizations in active members email

--- app/Console/Commands/SendActiveMembersEmail.php
+++ app/Console/Commands/SendActiveMembersEmail.php
@@ -46,7 +46,9 @@ class SendActiveMembersEmail extends Command
         // find authentications from the past month, if more than 3 add them to the list
         $email_data['member_list'] = [];
         foreach ($users as $user) {
-            $recs = \App\Models\Authentication::where('user_id', $user['id'])->where('gatekeeper_id', '11')->where('created_at', '>=', Carbon::now()->subMonth()->toDateTimeString())->get();
+            // Member entrance gatekeeper ID = 11
+            // Member entrance 2 backdoor gatekeeper ID = 28
+            $recs = \App\Models\Authentication::where('user_id', $user['id'])->where('gatekeeper_id', '11')->orWhere('gatekeeper_id', '28')->where('created_at', '>=', Carbon::now()->subMonth()->toDateTimeString())->get();
             if (count($recs) >= 3) {
                 $email_data['member_list'][] = $user['first_preferred'].' '.$user['last_preferred'];
             }
